bbfb41dbebc80353b4820deca44a9767f544c902
Michał Burzyński <michal.burzynski0805@gmail.com>
2024-09-04 18:44:47 +0200

    Added file loading and fixed minor errors

--- app.py
+++ app.py
@@ -5,25 +5,12 @@ import json
 import os
 from data import start_date, end_date, tickers
 from utils import download_stock_data
-
+from pathlib import Path
 
 stock_data = download_stock_data(tickers, start_date, end_date)
 stock_data_json = json.dumps(stock_data)
 
-assert os.path.exists('forecasts')
-
-# Get all predictions from forecasts/ directory
-for file in os.listdir('forecasts'):
-    print(file)
-
-app = dash.Dash(__name__)
-
-md_text = ""
-with open('desc.md', 'r') as file:
-    md_text = file.read()
-
-
-app.layout = html.Div([
+layout_data = [
     html.H1("Stockviz", style={"text-align": "center"}),
     dcc.Dropdown(
         id='stock-dropdown',
@@ -50,8 +37,45 @@ app.layout = html.Div([
         id='hidden-stock-data',
         style={'display': 'none'},
         children=stock_data_json
-    ),
-])
+    )
+]
+
+graph_input = [
+    dd.Input('hidden-stock-data', 'children'),
+    dd.Input('stock-dropdown', 'value'),
+    dd.Input('date-picker-range', 'start_date'),
+    dd.Input('date-picker-range', 'end_date'),
+    dd.Input('sma-checkbox', 'value')
+]
+
+
+assert os.path.exists('forecasts')
+
+# Get all predictions from forecasts/ directory
+for fn in os.listdir('forecasts'):
+    print(f"Adding data from {fn}")
+
+    with open(os.path.join('forecasts', fn)) as file:
+        layout_data.append(
+            html.Div(
+                id=f'pred-{Path(fn).stem}',
+                style={'display': 'none'},
+                children=json.dumps(json.load(file))
+            )
+        )
+
+        graph_input.append(
+            dd.Input(f'pred-{Path(fn).stem}', 'children')
+        )
+
+app = dash.Dash(__name__)
+
+md_text = ""
+with open('desc.md', 'r') as file:
+    md_text = file.read()
+
+
+app.layout = html.Div(layout_data)
 
 app.clientside_callback(
     ClientsideFunction(
@@ -60,12 +84,7 @@ app.clientside_callback(
     ),
 
     dd.Output('stock-graph', 'figure'),
-
-    [dd.Input('hidden-stock-data', 'children'),
-     dd.Input('stock-dropdown', 'value'),
-     dd.Input('date-picker-range', 'start_date'),
-     dd.Input('date-picker-range', 'end_date'),
-     dd.Input('sma-checkbox', 'value')]
+    graph_input
 )
 
 if __name__ == '__main__':
