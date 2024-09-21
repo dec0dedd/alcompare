9b4e7488227c46d7b05ec460cf824c49cbfcce7d
Michał Burzyński <michal.burzynski0805@gmail.com>
2024-09-21 21:28:13 +0200

    Changed VIS_LEN to 240 and PRED_LEN to 60

--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ import warnings
 warnings.filterwarnings('ignore')
 
 RND_INT = 1237  # random state in forecasting models
-PRED_LEN = 30  # number of days of prediction for forecasting models
-VIS_LEN = 120  # number of days used in visualization
+PRED_LEN = 60  # number of days of prediction for forecasting models
+VIS_LEN = 240  # number of days used in visualization
 
 """
 start_date/end_date - start/end date of all data
